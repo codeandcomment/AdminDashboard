a1fe2b9f7b2cded75210338d4926e93e8cc088cd
JB <infinityjaithra@gmail.com>
2025-03-09 20:27:16 +0530

    skeletion setup

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Dashboard from './Components/Dashboard/Dashboard'
+import Login from './Components/Login/Login'
+import Register from './Components/Register/Register'
+
+function App() {
+
+  return (
+    <>
+      <Dashboard></Dashboard>
+      <Login></Login>
+      <Register></Register>
+    </>
+  )
+}
+
+export default App
---
+++ client/src/Components/Register/Register.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Register = () =>{
+
+    return(
+        <>
+        Register
+        </>
+    )
+
+
+}
+
+export default Register;
